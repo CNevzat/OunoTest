4d09b0a5982fe57c7b216c0974f0914e14c1336b
CNevzat <ncirpicioglu@gmail.com>
2024-07-10 16:19:48 +0300

    V2

--- DataAccessLayer/ApplicationDbContext.cs
+++ DataAccessLayer/ApplicationDbContext.cs
@@ -12,5 +12,13 @@ public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : ba
 }
 
 public DbSet<Product> Products { get; set; }
+public DbSet<Category> Categories { get; set; }
 
+protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+    modelBuilder.Entity<Category>()
+    .HasMany(x => x.Products)
+    .WithOne(x => x.Category)
+    .HasForeignKey(x => x.CategoryId);
+}
 }
--- EntityLayer/Entities/Product.cs
+++ EntityLayer/Entities/Product.cs
@@ -2,11 +2,15 @@
 
 public class Product
 {
-public Guid ProductId { get; set; }
+public int Id { get; set; }
 
 public string Brand { get; set; }
 
 public string Model { get; set; }
 
 public int Price { get; set; }
+
+public int CategoryId { get; set; }
+
+public Category Category { get; set; }
 }
